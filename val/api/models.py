72ccaae3d23a94cda50cb1f61ed3de957c22f134
papaskripto <bygeoffreyouma@gmail.com>
2025-02-15 14:29:21 +0300

    add Book model

--- val/api/models.py
+++ val/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Book model
+class Book (models.Model):
+    book_title = models.charField (max_length=100)
+    author = models.CharField (max_lenght=40)
+    date_added = models.DateTimeField (auto_now_add=True)
+
+def __str__ (self):
+    return f'{self.book_title}, {self.author}, {self.date_added}'
