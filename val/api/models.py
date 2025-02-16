f29442e5576624ef835a90b141b3f968e6c97abe
papaskripto <bygeoffreyouma@gmail.com>
2025-02-16 13:22:46 +0300

    correct syntax errors

--- val/api/models.py
+++ val/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Book model
 class Book (models.Model):
-    book_title = models.charField (max_length=100)
-    author = models.CharField (max_lenght=40)
+    book_title = models.CharField (max_length=100)
+    author = models.CharField (max_length=40)
     date_added = models.DateTimeField (auto_now_add=True)
 
 def __str__ (self):
